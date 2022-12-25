182de5d9c6c11c147146582cc2589295b7b0cac9
IHR57 <iqbalhrasel95@gmail.com>
2022-12-25 17:50:50 +0600

    [SpringSecurity] - Added Custom Authentication Provider with Unit-test

---
+++ src/main/java/com/iqbal/securitybasic/services/MyUserNamePasswordAuthenticationProvider.java
@@ -0,0 +1,51 @@
+package com.iqbal.securitybasic.services;
+
+import com.iqbal.securitybasic.model.Customer;
+import com.iqbal.securitybasic.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserNamePasswordAuthenticationProvider implements AuthenticationProvider {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
+
+        String userName = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        List<Customer> customers = customerRepository.findByEmail(userName);
+        if(customers.size() > 0) {
+            if(passwordEncoder.matches(password, customers.get(0).getPwd())) {
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(userName, password, authorities);
+            } else {
+                throw new BadCredentialsException("Invalid Password");
+            }
+        } else {
+            throw new BadCredentialsException("User is not registered");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
